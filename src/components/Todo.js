9da09a29553a931709311bd5275864296f9e9596
Joseph Marks <jmichaelmarks@tutanota.com>
2023-07-30 21:57:15 -0400

    updates

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,6 +9,10 @@ function Todo(props) {
     setModalIsOpen(true);
   }
 
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
@@ -18,7 +22,7 @@ function Todo(props) {
         </button>
       </div>
       {modalIsOpen && <Modal />}
-      {modalIsOpen && <Backdrop />}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
 }
